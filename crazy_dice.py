c3d0cbaca5654d11e6007a0c5abee4a79453d80f
yaozezhen <63181901+yaozezhen@users.noreply.github.com>
2020-04-30 01:49:26 -0400

    Update crazy_dice.py

--- crazy_dice.py
+++ crazy_dice.py
@@ -1,11 +1,7 @@
-
-  
 import random 
 
 def roll_die(n): 
   return random.randint(1,n)
-
-# print (roll_die(1000), roll_die(1000))
 
 def trials(n,k): 
   num_dict = {}
@@ -16,8 +12,6 @@ def trials(n,k):
     num_dict[result] = num_dict[result] + 1
   for (key, value) in num_dict.items():
     print (str(key) + ": " + str(value) + ", probability: " + str(round(value/k*100, 2)) + "%")
-
-# trials(10, 10000)
 
 def crazy_dice(n,k): 
   num_dict = {}
@@ -30,9 +24,20 @@ def crazy_dice(n,k):
   for (key, value) in num_dict.items():
     print ("sum of " + str(key) + ": " + str(value) + " times, probability: " + str(round(value/k*100, 2)) + "%")
 
-crazy_dice(6,10000)
+def crazy_crazy_dice(x,n,k): 
+  num_dict = {}
+  for i in range(x,x*n+1):
+    num_dict[i] = 0
+  for i in range(k): 
+    result = 0
+    for j in range(x): 
+      result += roll_die(n)
+    num_dict[result] = num_dict[result] + 1
+  print ("Roll " + str(x) + " " + str(n) + "-sided dice " + str(k) + " times: \n")
+  for (key, value) in num_dict.items():
+    print ("sum of " + str(key) + ": " + str(value) + " times, probability: " + str(round(value/k*100, 2)) + "%")
 
-def crazy_crazy_dice(n,k): 
+def crazy_dice_permutation(n,k): 
   num_dict = {}
   for i in range(1, n+1): 
     for j in range(1,n+1): 
@@ -43,6 +48,3 @@ def crazy_crazy_dice(n,k):
     num_dict[result1, result2] = num_dict[result1, result2] +1
   for (key, value) in num_dict.items():
     print (str(key) + ": " + str(value) + ", probability: " + str(round(value/k*100, 2)) + "%")
-
-# crazy_dice(6,1000000)
-
