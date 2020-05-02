c940e6b6093ff9efd27cb2d0ebb032cb34a211ae
yaozezhen <63181901+yaozezhen@users.noreply.github.com>
2020-05-01 21:58:29 -0400

    Create eveningTheOdds.py

---
+++ eveningTheOdds.py
@@ -0,0 +1,40 @@
+import random 
+
+die_1 = [1, 2, 3, 4]
+die_2 = [1, 1, 1, 1]
+
+def eveningTheOdds(diceList,k): 
+  die1 = diceList[0]
+  die2 = diceList[1]
+  count = 0
+  sumDict = {}
+  for i in range(max(die1) + max(die2) + 1): 
+    sumDict[i] = 0
+  for i in range(k): 
+    outcome = random.choice(die1) + random.choice(die2)
+    sumDict[outcome] += 1
+    count += 1
+  for (key, value) in sorted(sumDict.items()): 
+    if value > 0: 
+      print ("sum of", key, ":", value, "times, probability:", round(int(value)/k*100, 2), "%")
+
+eveningTheOdds([die_1, die_2], 10000)
+
+def permutationETO(diceList,k): 
+  die1 = diceList[0]
+  die2 = diceList[1]
+  count = 0
+  sumDict = {}
+  for i in range(max(die1) + 1): 
+    for j in range(max(die2) + 1): 
+      sumDict[i,j] = 0
+  for i in range(k): 
+    outcome1 = random.choice(die1)
+    outcome2 = random.choice(die2)
+    sumDict[outcome1, outcome2] += 1
+    count += 1
+  for (key, value) in sumDict.items(): 
+    if value > 1: 
+      print ("sum of", key, ":", value, "times, probability:", round(value/k*100, 2), "%")
+
+permutationETO([die_1, die_2], 10000)
